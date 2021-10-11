6dd5291315e6761c1694f4471044790b93a277a1
Vivek Thazhathattil <vivek.thazhathattil@gmail.com>
2021-10-11 21:33:56 +0530

    add memory scripts

---
+++ memory/scores.py
@@ -0,0 +1,22 @@
+from matplotlib import pyplot as plt
+scores = [2, 3, 8, 6, 2, 4, 0, 16, 2, 2,
+        12, 5, 5, 16, 10, 11, 1, 9, 15, 5, 
+        12, 7, 1, 12, 17, 24, 0, 7, 17, 20,
+        10, 8]
+avg = round(sum(scores)/len(scores),2)
+cum_avg = []
+sum_ = 0
+for i in range(len(scores)):
+    sum_ += scores[i]
+    cum_avg.append(sum_/(i+1))
+
+print("Latest: " + str(scores[-1]))
+print("Average: " + str(avg))
+print("Number of tries: " + str(len(scores)))
+print("High score: " + str(max(scores)))
+plt.scatter([i for i in range(len(scores))], scores)
+plt.plot(scores)
+plt.plot(cum_avg)
+plt.legend(["Latest score (" + str(scores[-1]) + ")", "Average score (" + str(avg) + ") \n # of tries: " + str(len(scores)) + "\n High score: " + str(max(scores))])
+plt.grid();
+plt.show()
